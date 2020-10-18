4ca408e017803ac84e804d432fe3814704ed5f75
mohamed <med.elkhalki0@gmail.com>
2020-10-18 02:21:28 +0200

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 git config --global push.default matching
-git remote add deploy ssh://momo@$IP/$DEPLOY_DIR.git
+git remote add deploy ssh://momo@104.41.159.59/srv/git/myapp.git/
 git push deploy
