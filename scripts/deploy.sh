a4057fa96fce1936b24146df88d56450ef23fc07
mohamed <med.elkhalki0@gmail.com>
2020-10-18 04:16:47 +0200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #git config --global push.default matching
-git remote add deploy ssh://$USER@$HOST:$DEPLOY_DIR
+git remote add deploy ssh://$USER@$HOST/$DEPLOY_DIR
 #git push deploy --all
 git remote show deploy
