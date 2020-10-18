f82366eaf58be814899d5250b47f13bd2a679523
mohamed <med.elkhalki0@gmail.com>
2020-10-18 05:02:20 +0200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@
 git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/project.git
 #git push deploy --all
 git remote show deploy
-git push deploy main
-#git push --set-upstream deploy main
+#git push deploy main
+git push --set-upstream deploy main
