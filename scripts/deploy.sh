bf33025e32fb132293c8db61c952129935adedac
mohamed <med.elkhalki0@gmail.com>
2020-10-18 03:08:05 +0200

    fix travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git config --global push.default matching
-git remote add deploy ssh://$(USER)@$(HOST)/$(srv/git/myapp.git)
+git remote add deploy ssh://$USER@$HOST/$DEPLOY_DIR
 git remote show deploy
 git remote show origin
 git push deploy HEAD:master --force
