f58a2120112d0900d9bafbd4846432366886cc12
mohamed <med.elkhalki0@gmail.com>
2020-10-18 23:31:58 +0200

    nothing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-git config --global push.default matching
+git branch
 git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/project.git
 git remote show deploy
-git push deploy HEAD:test2
+git push deploy HEAD:test10
 #git push --quiet --set-upstream deploy HEAD:master
 #git push --set-upstream deploy main
