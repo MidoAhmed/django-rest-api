9858cdd13b10dc518fcfaa269595f78fc41f0a27
mohamed <med.elkhalki0@gmail.com>
2020-10-18 22:16:21 +0200

    improve deploy process 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,4 @@ git config --global push.default matching
 git remote add deploy ssh://$USER@$HOST/$DEPLOY_DIR
 git remote show deploy
 git fetch --unshallow deploy
-git push deploy HEAD:master
+git push deploy HEAD:refs/heads/master
