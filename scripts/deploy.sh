041e2c703c4622b34d1a8b1dcd194f768693f7de
mohamed <med.elkhalki0@gmail.com>
2020-10-18 03:32:14 +0200

    fix travis

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,4 @@ git config --global push.default matching
 git remote add deploy ssh://momo@$HOST/$DEPLOY_DIR
 git remote show deploy
 git remote show origin
-git fetch --unshallow deploy
-#git push deploy master --force
-git push deploy HEAD:refs/heads/master
+git push deploy --all --force
