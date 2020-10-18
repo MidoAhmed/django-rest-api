167c7d65210e95ef6ee72fb6b8618b255c6ddd43
mohamed <med.elkhalki0@gmail.com>
2020-10-19 00:41:09 +0200

    nothing

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ git branch
 git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/prj.git
 git remote show deploy
 git fetch --unshallow deploy
-git push deploy HEAD:refs/heads/master
+git push deploy HEAD:refs/heads/test10
 #git push --quiet --set-upstream deploy HEAD:master
 #git push --set-upstream deploy main
