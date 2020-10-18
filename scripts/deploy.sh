2172c31c95a6b2c181933b1333ed85d5acb3cdc0
mohamed <med.elkhalki0@gmail.com>
2020-10-18 04:26:40 +0200

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #git config --global push.default matching
-git remote add deploy ssh://git@104.41.159.59/home/momo/project.git
+git remote add deploy ssh://momo@104.41.159.59/home/momo/project.git
 #git push deploy --all
 git remote show deploy
