1538cb419b1a6be7044e1bb7d8fe7401f220007f
mohamed <med.elkhalki0@gmail.com>
2020-10-18 22:35:32 +0200

    improve deploy process 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,6 @@
 git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/project.git
 #git push deploy --all
 git remote show deploy
-git push deploy improve-deploy-process-7:master
+#git push deploy improve-deploy-process-7:master
+git push deploy --all
 #git push --set-upstream deploy main
