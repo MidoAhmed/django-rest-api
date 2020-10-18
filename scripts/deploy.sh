c8d838f0a021bfbdb589777a31f2a9148df93bfa
mohamed <med.elkhalki0@gmail.com>
2020-10-18 23:20:49 +0200

    improve deploy process 9

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,6 @@
 git config --global push.default matching
 git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/project.git
 git remote show deploy
-#git push deploy main:master
-git push --quiet --set-upstream deploy HEAD:master
+git push deploy HEAD:test2
+#git push --quiet --set-upstream deploy HEAD:master
 #git push --set-upstream deploy main
