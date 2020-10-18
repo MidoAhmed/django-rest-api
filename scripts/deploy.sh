6687d6f7b16ded5974445f21c19cf6174212a191
mohamed <med.elkhalki0@gmail.com>
2020-10-19 01:57:58 +0200

    test with empty github repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-#git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/prj.git
+git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/prj.git
 #git remote show deploy
 #git fetch --unshallow deploy
-#git branch -M master
-#git push -u deploy master
+git branch -M master
+git push -u deploy HEAD:refs/heads/master
 
-git remote add deploy https://github.com/MidoAhmed/test.git
-git branch -M main
-git push -u deploy HEAD:refs/heads/main
 
 
 #git push -u deploy HEAD:refs/heads/master
