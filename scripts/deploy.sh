fcd2c296ea1de824a5c11e79bb17159e618ccfad
mohamed <med.elkhalki0@gmail.com>
2020-10-19 16:50:17 +0200

    test scp

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,8 @@ setup_git() {
   git config --global user.name "Travis CI"
 }
 
-#setup_git
+setup_git
+
 #
 #git remote add production ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project
 #git remote show production
