fec85e3b6592d7f6f62e3e26695510370d07b821
mohamed <med.elkhalki0@gmail.com>
2020-10-19 02:41:58 +0200

    work-initiated-bare-repo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/prj.git
-#git remote show deploy
-git fetch --unshallow deploy
-#git branch -M master
-git push -u deploy HEAD:refs/heads/master
+git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/project.git
+#git fetch --unshallow deploy
+git push -u deploy HEAD:master
 
 
 
