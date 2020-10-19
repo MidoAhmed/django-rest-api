62435c048c20138d6fd281cf647ec0452d1a7c1b
mohamed <med.elkhalki0@gmail.com>
2020-10-19 03:55:05 +0200

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git remote add deploy ssh://momo@104.41.159.59/home/momo/bar-repository-folder/project.git
-git fetch --unshallow deploy
-git push -u deploy HEAD:refs/heads/main
-
-
+git remote add production ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project.git/
+#git fetch --unshallow deploy
+git add .
+git commit -m "Ready for production"
+git push production master
 
 #git push -u deploy HEAD:refs/heads/master
 #git push --quiet --set-upstream deploy HEAD:master
