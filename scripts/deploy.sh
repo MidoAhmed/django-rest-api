fe3f3add3241ca570f60109bc11087bad8ca0e5d
mohamed <med.elkhalki0@gmail.com>
2020-10-19 05:19:09 +0200

    skip_cleanup to cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,6 +9,5 @@ setup_git
 
 git remote add production ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project.git
 git remote show production
-
 git fetch --unshallow production
 git push production HEAD:refs/heads/main
