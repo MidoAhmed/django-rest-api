0bf4504fe6e6ded0d062fe60fa6b42ca5073abe6
mohamed <med.elkhalki0@gmail.com>
2020-10-19 04:16:32 +0200

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.org"
+  git config --global user.email "travis@travis-ci.com"
   git config --global user.name "Travis CI"
 }
 
@@ -11,5 +11,9 @@ setup_git
 git remote rm origin
 # Add new "origin"
 git remote add origin ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project.git > /dev/null 2>&1
+echo '---------'
 git branch
-git push origin main:master --quiet
+git branch -r
+git remote set-head origin master
+echo '---------'
+git push -u origin main:master --quiet
