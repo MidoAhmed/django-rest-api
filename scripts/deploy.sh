1528dc0862b07c80221149c3481c1e8d50cef10e
mohamed <med.elkhalki0@gmail.com>
2020-10-19 04:08:41 +0200

    update script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-git remote add production ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project.git
-git push -u production main:master
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
 
-#git push -u deploy HEAD:refs/heads/master
-#git push --quiet --set-upstream deploy HEAD:master
-#git push --set-upstream deploy main
+setup_git
+
+# Remove existing "origin"
+git remote rm origin
+# Add new "origin"
+git remote add origin ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project.git > /dev/null 2>&1
+git branch
+git push origin main:master --quiet
