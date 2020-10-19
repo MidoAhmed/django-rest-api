12b6f8090a37ef881fd3b2cacad8f7dca29454bf
mohamed <med.elkhalki0@gmail.com>
2020-10-19 04:45:31 +0200

    skip_cleanup to cleanup

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 setup_git() {
-  git config --global user.email "travis@travis-ci.com"
+  git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
 }
 
 setup_git
 
 git remote add production ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project.git
-git push -u production main:master
+git remote show production
+git push production --all
