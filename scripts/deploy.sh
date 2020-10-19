9e6d04e69ac8bf7463f05784870550024f5a4d60
mohamed <med.elkhalki0@gmail.com>
2020-10-19 18:15:49 +0200

    clean up deploy script & add post-receive hook

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,26 +7,6 @@ setup_git() {
 
 setup_git
 
-#
-#git remote add production ssh://momo@104.41.159.59:22/home/momo/bar-repository-folder/project
-#git remote show production
-#git fetch --unshallow production
-#git push production main:refs/heads/master
-
-#pwd
-##scp -r scripts momo@104.41.159.59:~/.
-#cd ..
-#pwd
-#git clone ssh://momo@104.41.159.59/~/git/djangoRestApi.git testproject
-#ls
-#cd testproject
-#git status
-#touch test.txt
-#git add .
-#git commit -m 'init commit'
-#git push origin main:master
-
 git remote add production ssh://momo@104.41.159.59/~/git/djangoRestApi.git
 git remote show production
-#git fetch --unshallow production
 git push production main:master
